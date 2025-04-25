32748904ddc6faddb5b13555cd76b67f3cd5238e
alkeity <alkeity.23@gmail.com>
2025-04-25 03:31:46 +0300

    hw2

---
+++ Classwork_ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Classwork_ConsoleApp
+{
+    interface IMusic
+    {
+        public string GetGenre();
+    }
+
+    class ClassicalMusic : IMusic
+    {
+        public string GetGenre()
+        {
+            return "Classical";
+        }
+    }
+
+    class RockMusic : IMusic
+    {
+        public string GetGenre()
+        {
+            return "Rock";
+        }
+    }
+
+    class FolkMusic : IMusic
+    {
+        public string GetGenre()
+        {
+            return "Fplk";
+        }
+    }
+
+    class MusicPlayer
+    {
+        private readonly IMusic _music;
+
+        public MusicPlayer(IMusic music)
+        {
+            _music = music;
+        }
+        public void PlayMusic()
+        {
+            Console.WriteLine($"[Music Player] Playing {_music.GetGenre()} music...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Extensions.DependencyInjection.IServiceCollection services = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+
+            IMusic music = new RockMusic();
+            MusicPlayer player = new MusicPlayer(music);
+            player.PlayMusic();
+        }
+    }
+}
---
+++ Homework2_DependencyInjection/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Homework2_DependencyInjection
+{
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    interface IMusic
+    {
+        string GetGenre();
+    }
+
+    class Music : IMusic
+    {
+        protected string _genre = string.Empty;
+
+        public string GetGenre()
+        {
+            return _genre;
+        }
+    }
+
+    class FolkMusic : Music
+    {
+        public FolkMusic()
+        {
+            _genre = "Folk";
+        }
+    }
+
+    class RockMusic : Music
+    {
+        public RockMusic()
+        {
+            _genre = "Rock";
+        }
+    }
+
+    class MusicPlayer
+    {
+        IMusic _music;
+        ILogger _logger;
+
+        public MusicPlayer(IMusic music, ILogger logger)
+        {
+            _music = music;
+            _logger = logger;
+        }
+
+        public void PlayMusic()
+        {
+            _logger.Log($"Now playing: {_music.GetGenre()} music.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddSingleton<ILogger, ConsoleLogger>();
+            services.AddSingleton<IMusic, FolkMusic>();
+            services.AddSingleton<MusicPlayer>();
+
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            MusicPlayer player = serviceProvider.GetRequiredService<MusicPlayer>();
+            player.PlayMusic();
+        }
+    }
+}
