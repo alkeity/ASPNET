3efbd7fe4dd81b1e33fb580ab04d3aa699146284
alkeity <alkeity.23@gmail.com>
2025-05-21 00:24:26 +0300

    hw9

--- App_ToDoList/Pages/Tasks/TaskEdit.cshtml.cs
+++ App_ToDoList/Pages/Tasks/TaskEdit.cshtml.cs
@@ -7,6 +7,13 @@ namespace App_ToDoList.Pages.Tasks
     public class TaskEditModel : PageModel
     {
         private readonly ITaskService _taskService;
+
+        [BindProperty]
+        public required string Title { get; set; }
+
+        [BindProperty]
+        public string? Description { get; set; }
+
         public TaskEditModel(ITaskService taskService)
         {
             _taskService = taskService;
@@ -17,7 +24,7 @@ namespace App_ToDoList.Pages.Tasks
 
         public IActionResult OnPost()
         {
-            _taskService.CreateTask(Request.Form["title"], Request.Form["description"]);
+            _taskService.CreateTask(Title, Description);
             return RedirectToPage("TaskList");
         }
     }
